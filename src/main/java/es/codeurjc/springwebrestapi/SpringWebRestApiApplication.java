490999743dacec306b9cf57c1f8738b7019dae73
Javier García González <javier@garciagon.com>
2020-12-06 09:52:40 +0100

    Initial commit

---
+++ src/main/java/es/codeurjc/springwebrestapi/SpringWebRestApiApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.springwebrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/codeurjc/springwebrestapi/SpringWebRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.springwebrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
